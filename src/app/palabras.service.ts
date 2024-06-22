b5fe5ee9fab65b04b7a7934d910650747e6e1ac6
avillaq <villafuertequispealex@gmail.com>
2024-06-22 12:06:29 -0500

    Añadimos el arreglo de palabras relacionadas a la programacion en el servicio

--- src/app/palabras.service.ts
+++ src/app/palabras.service.ts
@@ -4,5 +4,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PalabrasService {
+  // Palabras relacionadas con la programacion 
+  private palabras: string[] = ['programacion', 'javascript', 'typescript', 'angular', 'nodejs', 'express', 'mysql', 'php', 'python', 'java', 'perl','android', 'windows', 'linux'];
+
+  getPalabras(){
+    return this.palabras;
+  }
 
 }
