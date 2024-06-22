e5cad887f418aa1ba656e32c3d59297bd36be340
avillaq <villafuertequispealex@gmail.com>
2024-06-22 12:18:18 -0500

    Se crea otro atributo arreglo para hacer un seguimiento a las letras que el usuario ingrese. Al comienzo, este arreglo seran guiones bajos "_" de acuerdo a la palabra aleatoria.

--- src/app/tablero/tablero.component.ts
+++ src/app/tablero/tablero.component.ts
@@ -11,10 +11,15 @@ import { PalabrasService } from "../palabras.service"
 export class TableroComponent {
   listaPalabras:string[] = [];
   palabraAleatoria:string = "";
+  palabraUsuario:string[] = []
 
   constructor(private palabras: PalabrasService){
     this.listaPalabras = palabras.getPalabras();
     this.palabraAleatoria = this.listaPalabras[Math.floor(Math.random()*4)]
+    
+    for (let i = 0; i < this.palabraAleatoria.length; i++) {
+      this.palabraUsuario.push("_")
+    }
 
   }
 
