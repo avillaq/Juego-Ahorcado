d5cefca217da3d46d6e530085c1a3decbd8b81f4
avillaq <villafuertequispealex@gmail.com>
2024-06-22 17:26:48 -0500

    Creamos variables para controlar el numero de intentos, para obtener una images segun los intentos y establecer la imagen.

--- src/app/imagenes.service.ts
+++ src/app/imagenes.service.ts
@@ -6,4 +6,19 @@ import { Injectable } from '@angular/core';
 export class ImagenesService {
   // Imagenes del ahorcado
   private imagenes: string[] = ['../assets/img/1.png', '../assets/img/2.png', '../assets/img/3.png', '../assets/img/4.png', '../assets/img/5.png', '../assets/img/6.png'];
+
+  tamaño = this.imagenes.length;
+  posActual = 0;
+
+  getIntentos(){
+    return this.tamaño;
+  }
+
+  getImagen(){
+    return this.imagenes[this.posActual];
+  }
+  setImagenActual(pos : number){
+    this.posActual = this.tamaño-pos;
+  }
+
 }
