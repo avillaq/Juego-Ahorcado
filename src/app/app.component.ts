b9fb2636b802386715765ed5328fc78946deb140
avillaq <villafuertequispealex@gmail.com>
2024-06-22 11:59:43 -0500

    Importamos tablero en el componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TableroComponent } from './tablero/tablero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TableroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
